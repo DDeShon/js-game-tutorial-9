066b27bdb6153f4aabd7a953a3312746abd31ca8
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-06 18:51:02 -0500

    creating enemy class

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 800;
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 800;
 
-class Game {
-  constructor() {
-    this.enemies = [];
+  class Game {
+    constructor() {
+      this.enemies = [];
+    }
+    update() {}
+    draw() {}
+    #addNewEnemy() {}
   }
-  update() {}
-  draw() {}
-  #addNewEnemy() {}
-}
 
-class Enemy {
-  constructor() {}
-  update() {}
-  draw() {}
-}
+  class Enemy {
+    constructor() {
+      this.x = 100;
+      this.y = 100;
+      this.width = 100;
+      this.height = 100;
+    }
+    update() {
+      this.x--;
+    }
+    draw() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+  let lastTime = 1;
+  function animate(timeStamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    requestAnimationFrame(animate);
+  }
+
+  animate(0);
+});

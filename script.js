57978169fd651d474eeb6921f83ab0dd2fefebeb
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-08 22:10:40 -0500

    working on enemy class

--- script.js
+++ script.js
@@ -44,16 +44,27 @@ document.addEventListener("DOMContentLoaded", function () {
     constructor(game) {
       this.game = game;
       this.markedForDeletion = false;
+      this.frameX = 0;
+      this.maxFrame = 5;
+      this.frameInterval = 100;
+      this.frameTimer = 0;
     }
     update(deltaTime) {
       this.x -= this.xVelocity * deltaTime;
       // remove enemies
       if (this.x < 0 - this.width) this.markedForDeletion = true;
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX < this.maxFrame) this.frameX++;
+        else this.frameX = 0;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
     }
     draw(ctx) {
       ctx.drawImage(
         this.image,
-        0,
+        this.frameX * this.spriteWidth,
         0,
         this.spriteWidth,
         this.spriteHeight,
